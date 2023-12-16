ed0b3c975e892e77770ad5359ea5d3af5d0b92fc
JeuryQ-M-2022-0621 <jeuryq3@gmail.com>
2023-12-16 16:43:43 -0400

    first commit

---
+++ practica-final-devops-JEURY QUEZADA/index.test.js
@@ -0,0 +1,18 @@
+// index.test.js
+
+describe('Test Hola Mundo', () => {
+  test('should pass', () => {
+    if (typeof document !== 'undefined') {
+      const body = document.createElement('body');
+      const h1 = document.createElement('h1');
+      h1.textContent = 'Hola Mundo';
+      body.appendChild(h1);
+
+      expect(document.body.textContent).toContain('Hola Mundo');
+    } else {
+      // Manejar el caso cuando se ejecuta fuera del navegador o entorno jsdom
+      // Por ejemplo, puedes imprimir un mensaje de advertencia
+      console.warn('No se pudo acceder al objeto document. Asegúrate de que estás ejecutando en un entorno adecuado.');
+    }
+  });
+});
